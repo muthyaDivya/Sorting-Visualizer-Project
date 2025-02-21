44222a9557639870524b7fbbd7fae4b4a19b46d2
muthyaDivya <muthya.divya@gmail.com>
2025-02-21 12:03:20 +0000

    specified speed value dynamically in these sorting algos

--- insertion.js
+++ insertion.js
@@ -5,14 +5,14 @@ document.getElementById("insertion").addEventListener("click", async () => {
 });
 
 async function insertionSort(arr){
-    
+    const speed = document.querySelector("#sort_speed").value;
     for(let i = 1; i < arr.length; i++){
         arr[i].style.background = "red"
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Delay to visualize
+        await new Promise(resolve => setTimeout(resolve, speed)); // Delay to visualize
         let temp = arr[i].style.height;
         let j = i-1;
         arr[j].style.background = "red"
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Delay to visualize
+        await new Promise(resolve => setTimeout(resolve, speed)); // Delay to visualize
         while(j >= 0 && parseInt(arr[j].style.height) > parseInt(temp)){
             arr[j + 1].style.height = arr[j].style.height;
             arr[j].style.background = "green";
--- quick.js
+++ quick.js
@@ -7,6 +7,7 @@ document.getElementById("quick").addEventListener("click", async () => {
 });
 
 async function quickSort(arr, start, end){
+    
     if(end <= start) return;
     let pivot = await partition(arr, start, end);
     await quickSort(arr, start, pivot - 1);
@@ -14,16 +15,17 @@ async function quickSort(arr, start, end){
 
 }
 async function partition(arr, start, end){
+    const speed = document.querySelector("#sort_speed").value;
     arr[end].style.background = "blue";
     let pivot = parseInt(arr[end].style.height);
     let i = start - 1;
     for(let j = start; j <= end - 1; j++){
         arr[j].style.background = "red";
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, speed));
         if(parseInt(arr[j].style.height) < pivot){
             i++;
             arr[i].style.background = "orange";
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            await new Promise(resolve => setTimeout(resolve, speed));
             let temp = arr[i].style.height;
             
             arr[i].style.height = arr[j].style.height;
@@ -40,7 +42,7 @@ async function partition(arr, start, end){
     arr[end].style.height = temp;
 
     arr[i].style.background = "green";
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => setTimeout(resolve, speed));
     for(let k=0;k<arr.length;k++){
         arr[k].style.background = "green";
     }
